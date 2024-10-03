495d10d6440a4881884b94b5b6713b6789892167
Thanh Nguyen <thanh.nguyen@nexpando.com>
2024-10-03 07:19:30 +0700

    hotfix

--- src/components/buttons/ScrollToTopButton.tsx
+++ src/components/buttons/ScrollToTopButton.tsx
@@ -13,7 +13,7 @@ export const ScrollToTopButton = () => {
 
   return (
     <Tooltip title="Scrol to top" onClick={() => handleScroll()}>
-      <Avatar className="z-50 fixed bottom-5 right-5 bg-blue-100 border border-blue-200 rounded-full animate-bounce size-12 cursor-pointer">
+      <Avatar className="z-50 !fixed bottom-5 right-5 !bg-blue-100 border !border-blue-200 rounded-full animate-bounce size-12 cursor-pointer">
         <ExpandLessIcon fontSize="large" color="primary" />
       </Avatar>
     </Tooltip>
