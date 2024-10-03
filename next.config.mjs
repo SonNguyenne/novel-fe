1abe9d09797c5cce9d3d2a86ece362b50b0180aa
SonNguyenne <ts29032001@gmail.com>
2024-10-03 18:10:38 +0700

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-  },
+  // env: {
+  //   API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  // },
 }
 
 export default nextConfig
