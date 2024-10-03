a8ce5d3fed8bd1fc575bed5c6d36416e5b73afb0
Thanh Nguyen <thanh.nguyen@nexpando.com>
2024-10-03 11:34:57 +0700

    hotfix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,25 +10,21 @@ const NotFound = () => {
     <Box
       sx={{
         display: 'flex',
-        direction: 'column',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
       }}
       className="flex-1"
     >
-      <Grid container spacing={2} className="flex flex-col justify-center items-center gap-4">
-        <Grid xs={6} className="lg:order-1 flex flex-col justify-center items-center">
-          <Typography variant="h1">404</Typography>
-          <Typography variant="h6" className="text-center">
-            The page you’re looking for doesn’t exist.
-          </Typography>
-          <Link href="/">
-            <Button variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Back Home
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
+      <Typography variant="h1">404</Typography>
+      <Typography variant="h6" className="text-center">
+        The page you’re looking for doesn’t exist.
+      </Typography>
+      <Link href="/">
+        <Button variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Back Home
+        </Button>
+      </Link>
     </Box>
   )
 }
