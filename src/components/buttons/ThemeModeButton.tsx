a0e9e522f500535e0963d04b4602ab4681259dbb
Thanh Nguyen <thanh.nguyen@nexpando.com>
2024-10-16 15:01:26 +0700

    fix theme provider and btn

--- src/components/buttons/ThemeModeButton.tsx
+++ src/components/buttons/ThemeModeButton.tsx
@@ -4,18 +4,21 @@ import React from 'react'
 
 export const ThemeModeButton = () => {
   const { mode, setMode } = useColorScheme()
-  if (!mode) setMode('light')
+  if (!mode) {
+    setMode('light')
+    return null
+  }
 
-  return mode === 'light' ? (
-    <Tooltip title="Light mode" onClick={() => setMode('dark')}>
-      <IconButton color="primary" size="small">
-        <WbSunny />
-      </IconButton>
-    </Tooltip>
-  ) : (
+  return mode === 'dark' ? (
     <Tooltip title="Dark mode" onClick={() => setMode('light')}>
       <IconButton size="small">
         <DarkMode />
+      </IconButton>
+    </Tooltip>
+  ) : (
+    <Tooltip title="Light mode" onClick={() => setMode('dark')}>
+      <IconButton color="primary" size="small">
+        <WbSunny />
       </IconButton>
     </Tooltip>
   )
